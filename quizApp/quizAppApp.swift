28862fb39d2f0ac7ee402da8052c80999a9d4ebd
Ava Ginsberg <avaginsberg08@gmail.com>
2023-07-28 10:18:55 -0400

    Initial Commit

---
+++ quizApp/quizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizAppApp.swift
+//  quizApp
+//
+//  Created by scholar on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct quizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
